a1b52bd0472b9f7aadc9ef830e99817b4509f8f2
Angel Gallego <angelgb1990@gmail.com>
2024-09-01 00:41:42 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#excusa").innerHTML = generateExcuse();
+  console.log("Hello Rigo from the console! ");
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let pro = ["Mi", "El", "Tu"];
+  let quien = [" perro", " gato", " loro", " raton"];
+  let verbo = [" se ha comido", " ha cogido", " se ha llevado"];
+  let objetivo = [" mi teclado", " mi libro", " tus deberes", " mi movil"];
+  let donde = [" en casa", " en la biblioteca", " a la carretera"];
+
+  let pro1 = Math.floor(Math.random() * pro.length);
+  let pro2 = Math.floor(Math.random() * quien.length);
+  let pro3 = Math.floor(Math.random() * verbo.length);
+  let pro4 = Math.floor(Math.random() * objetivo.length);
+  let pro5 = Math.floor(Math.random() * donde.length);
+
+  return pro[pro1] + quien[pro2] + verbo[pro3] + objetivo[pro4] + donde[pro5];
 };
